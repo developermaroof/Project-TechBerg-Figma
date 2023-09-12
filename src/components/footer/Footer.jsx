d63a21098e3131f18bee10f3d1d86e07907d6c5e
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2023-09-12 13:15:52 +0500

    Updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import "./footer.css";
+import github from "../../images/github.png";
+import linkedin from "../../images/linkedin.png";
+import facebook from "../../images/facebook.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <hr />
+      <div className="footer-div">
+        <p>
+          Developed by: <span>Maroof Qureshi</span>
+        </p>
+
+        <img src={github} alt="icon" />
+
+        <img src={linkedin} alt="icon" />
+
+        <img src={facebook} alt="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
