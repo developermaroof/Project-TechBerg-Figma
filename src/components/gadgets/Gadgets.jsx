311a2b83b7e96219d4a836b29d1a7fbd0174e853
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2023-09-18 14:11:11 +0500

    Updated

--- src/components/gadgets/Gadgets.jsx
+++ src/components/gadgets/Gadgets.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./gadgets.css";
+
 import Navbar from "../navbar/Navbar";
 import Footer from "../footer/Footer";
 import Gadgets_1 from "../../images/gadgets-1.png";
@@ -8,99 +9,97 @@ import Gadgets_3 from "../../images/gadgets-3.png";
 
 const Gadgets = () => {
   return (
-    <div className="container">
-      <Navbar />
-      <div className="heading">
-        <h2>GADGETS</h2>
-      </div>
-      <div className="bar"></div>
-      {/* 1st Gadget */}
-      <div className="gadget-1">
-        <div className="img">
-          <img src={Gadgets_1} alt="img" />
+    <div className="App">
+      <div className="container">
+        <Navbar />
+        <div className="heading">
+          <h2>GADGETS</h2>
         </div>
-        <div className="LatestPosts">
-          <div className="newsletter">
-            <h2>Newsletter</h2>
-            <div className="bar"></div>
-            <p>Enter your email address below to subscribe to my newsletter</p>
-            <input type="text" placeholder="Your email address.." />
-            <button>SUBSCRIBE</button>
+        <div className="head-bar"></div>
+        {/* 1st Gadget */}
+        <div className="gadget-1">
+          <div className="img">
+            <img src={Gadgets_1} alt="img" />
+            <span>GADGETS</span>
+            <h2>Explore the Sea world with Deep Sea Fishing Miami 2018 </h2>
+            <p className="date">May 15 2018</p>
+
+            <p className="info">
+              More off this less hello salamander lied porpoise much over
+              tightly circa horse taped so innocuously outside crud mightily
+              rigorous…
+            </p>
+
+            <p className="readmore">Read More</p>
           </div>
-          <h3>Latest Posts</h3>
-          <hr />
-          <p className="data">
-            <span>Gadgets</span>
-            <br /> <b>
-              How i went from programming with a Nokia to Samsung !
-            </b>{" "}
-          </p>
-          <p className="date">May 15 2018</p>
-          <p className="data">
-            <span>Gadgets</span>
-            <br />
-            <b>How i went from programming with a Nokia to Samsung !</b>{" "}
-          </p>
-          <p className="date">May 15 2018</p>
-          <p className="data">
-            <span>Gadgets</span>
-            <br />
-            <b>How i went from programming with a Nokia to Samsung !</b>{" "}
-          </p>
-          <p className="date">May 15 2018</p>
+          <div className="LatestPosts">
+            <div className="newsletter">
+              <h2>Newsletter</h2>
+              <div className="bar"></div>
+              <p>
+                Enter your email address below to subscribe to my newsletter
+              </p>
+              <input type="text" placeholder="Your email address.." />
+              <button>SUBSCRIBE</button>
+            </div>
+            <h3>Latest Posts</h3>
+            <hr />
+            <p className="data">
+              <span>Gadgets</span>
+              <br />{" "}
+              <b>How i went from programming with a Nokia to Samsung !</b>{" "}
+            </p>
+            <p className="date">May 15 2018</p>
+            <p className="data">
+              <span>Gadgets</span>
+              <br />
+              <b>How i went from programming with a Nokia to Samsung !</b>{" "}
+            </p>
+            <p className="date">May 15 2018</p>
+            <p className="data">
+              <span>Gadgets</span>
+              <br />
+              <b>How i went from programming with a Nokia to Samsung !</b>{" "}
+            </p>
+            <p className="date">May 15 2018</p>
+          </div>
         </div>
-        <div className="gadget-1_info">
-          <span>GADGETS</span>
-          <h2>Explore the Sea world with Deep Sea Fishing Miami 2018 </h2>
-          <p className="date">May 15 2018</p>
+        {/* 2nd Gadget */}
+        <div className="gadget-2">
+          <div className="img">
+            <img src={Gadgets_2} alt="img" />
+            <span>GADGETS</span>
+            <h2>3 reasons to get excited about a brand new greenhouse</h2>
+            <p className="date">May 15 2018</p>
 
-          <p className="info">
-            More off this less hello salamander lied porpoise much over tightly
-            circa horse taped so innocuously outside crud mightily rigorous…
-          </p>
+            <p className="info">
+              More off this less hello salamander lied porpoise much over
+              tightly circa horse taped so innocuously outside crud mightily
+              rigorous…
+            </p>
 
-          <p className="readmore">Read More</p>
+            <p className="readmore">Read More</p>
+          </div>
         </div>
+        {/* 3rd Gadget */}
+        <div className="gadget-3">
+          <div className="img">
+            <img src={Gadgets_3} alt="img" />
+            <span>GADGETS</span>
+            <h2>Archaeologists may find a hidden tunnel in king Tut’s tomb </h2>
+            <p className="date">May 15 2018</p>
+
+            <p className="info">
+              More off this less hello salamander lied porpoise much over
+              tightly circa horse taped so innocuously outside crud mightily
+              rigorous…
+            </p>
+
+            <p className="readmore">Read More</p>
+          </div>
+        </div>
+        <Footer />
       </div>
-      {/* 2nd Gadget */}
-      <div className="gadget-2">
-        <div className="img">
-          <img src={Gadgets_2} alt="img" />
-        </div>
-
-        <div className="gadget-2_info">
-          <span>GADGETS</span>
-          <h2>3 reasons to get excited about a brand new greenhouse</h2>
-          <p className="date">May 15 2018</p>
-
-          <p className="info">
-            More off this less hello salamander lied porpoise much over tightly
-            circa horse taped so innocuously outside crud mightily rigorous…
-          </p>
-
-          <p className="readmore">Read More</p>
-        </div>
-      </div>
-      {/* 3rd Gadget */}
-      <div className="gadget-3">
-        <div className="img">
-          <img src={Gadgets_3} alt="img" />
-        </div>
-
-        <div className="gadget-3_info">
-          <span>GADGETS</span>
-          <h2>Archaeologists may find a hidden tunnel in king Tut’s tomb </h2>
-          <p className="date">May 15 2018</p>
-
-          <p className="info">
-            More off this less hello salamander lied porpoise much over tightly
-            circa horse taped so innocuously outside crud mightily rigorous…
-          </p>
-
-          <p className="readmore">Read More</p>
-        </div>
-      </div>
-      <Footer />
     </div>
   );
 };
